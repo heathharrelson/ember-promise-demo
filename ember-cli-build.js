47849464d67bc2de38853cae98a3bc7459536d4d
Heath Harrelson <harrelst@ohsu.edu>
2016-11-11 11:48:31 -0800

    Add Font Awesome
    Add Font Awesome icon font for use in loading substate demo.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,26 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+"use strict";
+
+let EmberApp = require('ember-cli/lib/broccoli/ember-app');
+let funnel = require('broccoli-funnel');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  let fontAwesomeFonts = funnel('bower_components/font-awesome/fonts', {
+    srcDir: '/',
+    destDir: '/fonts'
+  });
 
-  return app.toTree();
+  let fontAwesomeCss = funnel('bower_components/font-awesome/css', {
+    srcDir: '/',
+    destDir: '/assets'
+  });
+
+  let treesToMerge = [fontAwesomeFonts, fontAwesomeCss];
+
+  return app.toTree(treesToMerge);
 };
